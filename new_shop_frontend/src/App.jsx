b27e2a83787ad70ad74776b904e57e45f4356dcd
wlodzimierrr <140817588+wlodzimierrr@users.noreply.github.com>
2024-06-13 22:31:35 +0100

    first layout of the website, set up diirectory structure and added react router routes

--- new_shop_frontend/src/App.jsx
+++ new_shop_frontend/src/App.jsx
@@ -1,9 +1,36 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import './App.css'
+
+// Components
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+
+
+// Pages
+import Home from "./pages/Home"
+import Category from "./pages/Category" 
+import Product from "./pages/Product"
 
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+  <main className='text-tertiary'>
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/category" element={<Category />}/>
+        <Route path="/category" element={<Category />}/>
+        <Route path="/category" element={<Category />}/>
+        <Route path="/product" element={<Product />}>
+          <Route path=":productId" element={<Product />}/>
+        </Route>
+        {/* <Route path="/cart-page" element={<Cart />}/>
+        <Route path="/login" element={<Login />}/> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  </main>
   )
 }
