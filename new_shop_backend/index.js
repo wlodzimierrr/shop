3ba49397f96435905d9370714d36c81f27870587
wlodzimierrr <140817588+wlodzimierrr@users.noreply.github.com>
2024-06-21 10:24:14 +0100

    add Helmet, rate limiting, and compression middleware

--- new_shop_backend/index.js
+++ new_shop_backend/index.js
@@ -1,32 +1,63 @@
 const express = require('express');
 const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const rateLimit = require('express-rate-limit');
+const { PORT } = require('./config');
 
+// Initialize express app
 const app = express();
 
+// Middleware setup
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
+app.use(compression());
 
-const { PORT } = require('./config');
+// Content Security Policy
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      defaultSrc: ["'self'"],
+      scriptSrc: ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+      styleSrc: ["'self'", "'unsafe-inline'", "cdn.jsdelivr.net"],
+      imgSrc: ["'self'", "data:", "cdn.jsdelivr.net"],
+      fontSrc: ["'self'", "cdn.jsdelivr.net"],
+      connectSrc: ["'self'"],
+      frameSrc: ["'self'"],
+      objectSrc: ["'none'"],
+      upgradeInsecureRequests: [],
+    },
+  })
+);
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20, // limit each IP to 20 requests per windowMs
+  message: 'Too many requests from this IP, please try again after 1 minute'
+});
+app.use(limiter);
 
 // Routes
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
-const ordersRoutes = require('./routes/ordersRoutes')
-const userRoutes = require('./routes/userRoutes')
+const ordersRoutes = require('./routes/ordersRoutes');
+const userRoutes = require('./routes/userRoutes');
 
 // Custom error handler
-const errorHandler = require('./middleware/errorHandlingMiddleware')
+const errorHandler = require('./middleware/errorHandlingMiddleware');
 app.use(errorHandler);
 
 // Image storage engine
 const upload = require('./utility/multer');
 app.use('/images', express.static('uploads/images'));
 app.post('/api/upload', upload.single('product'), (req, res) => {
-    res.json({
-        success: 1,
-        image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
-    });
+  res.json({
+    success: 1,
+    image_url: `http://localhost:${PORT}/images/${req.file.filename}`,
+  });
 });
 
 // Route handlers
@@ -36,5 +67,5 @@ app.use('/api/cart', cartRoutes);
 app.use('/api/orders', ordersRoutes);
 app.use('/api/user', userRoutes);
 
-
+// Start the server
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
